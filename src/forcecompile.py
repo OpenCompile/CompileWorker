07f72272f4eaa3dc133087bc11ae7261399974f9
Daniel Hejduk <jellymail@protonmail.com>
2023-10-18 19:09:03 +0200

    Adding arch detection

--- src/forcecompile.py
+++ src/forcecompile.py
@@ -2,10 +2,13 @@ from srcmanage import SourceApp
 from subprocess import call
 import requests
 import json
+import platform
 """
     I2PD
     building script
 """
+
+arch = platform.machine()
 
 l = json.loads(open("BuildScripts/list.json", "r").read())
 
@@ -16,6 +19,6 @@ xmrig.response = xmrig.response.json()["name"]
 
 response = requests.get(f"https://api.github.com/repos/xmrig/xmrig/releases/latest")
 call(f"BuildScripts/xmrig/xmrig/build.sh", shell=True)
-call(f"cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}.tar.gz", shell=True)
+call(f"cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz", shell=True)
 call(f"sha256sum TarRepo/xmrig/xmrig/xmrig{xmrig.response}.tar.gz >> TarRepo/xmrig/xmrig/SHA256SUMS.txt", shell=True)
 call(f'cd TarRepo && git pull origin main && git add . && git commit -m "Updating xmrig to {xmrig.response}" && git push origin main', shell=True)
--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import os
 import json
 from subprocess import call
 from srcmanage import SourceApp
+import platform
 
 def main():
 
     sowner = "OpenCompile"
     srepo = "BuildScripts"
+    arch = platform.machine()
 
     if os.path.exists("BuildScripts"):
         l = json.loads(open("BuildScripts/list.json", "r").read())
@@ -15,7 +17,7 @@ def main():
         xmrig = SourceApp(l["xmrig"])
         xmrig.repo_exists(xmrig.repo)
         xmrig.checkver(xmrig.repo)
-        call(f"cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}.tar.gz", shell=True)
+        call(f"cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz", shell=True)
         call(f"sha256sum TarRepo/xmrig/xmrig/xmrig{xmrig.response}.tar.gz >> TarRepo/xmrig/xmrig/SHA256SUMS.txt", shell=True)
         call(f'cd TarRepo && git pull origin main && git add . && git commit -m "Updating xmrig to {xmrig.response}" && git push origin main', shell=True)
     else:
