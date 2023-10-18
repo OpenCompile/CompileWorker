594057d185a1fe43c56f0d6d4e50484dff5cc62a
Daniel Hejduk <jellymail@protonmail.com>
2023-10-18 19:10:39 +0200

    Adding arch detection FIX

--- src/forcecompile.py
+++ src/forcecompile.py
@@ -20,5 +20,5 @@ xmrig.response = xmrig.response.json()["name"]
 response = requests.get(f"https://api.github.com/repos/xmrig/xmrig/releases/latest")
 call(f"BuildScripts/xmrig/xmrig/build.sh", shell=True)
 call(f"cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz", shell=True)
-call(f"sha256sum TarRepo/xmrig/xmrig/xmrig{xmrig.response}.tar.gz >> TarRepo/xmrig/xmrig/SHA256SUMS.txt", shell=True)
+call(f"sha256sum TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz >> TarRepo/xmrig/xmrig/SHA256SUMS.txt", shell=True)
 call(f'cd TarRepo && git pull origin main && git add . && git commit -m "Updating xmrig to {xmrig.response}" && git push origin main', shell=True)
