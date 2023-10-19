aea9d176ebe73b3a755a4112346624d4ba0b553a
Daniel Hejduk <jellymail@protonmail.com>
2023-10-19 19:17:32 +0200

    Removing tmp after each run

--- src/main.py
+++ src/main.py
@@ -17,9 +17,10 @@ def main():
         xmrig = SourceApp(l["xmrig"])
         xmrig.repo_exists(xmrig.repo)
         xmrig.checkver(xmrig.repo)
-        call(f"cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz", shell=True)
+        call(f"mkdir -p TarRepo/xmrig/xmrig && cp Repos/xmrig/xmrig/build/xmrig.tar.gz TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz", shell=True)
         call(f"sha256sum TarRepo/xmrig/xmrig/xmrig{xmrig.response}-{arch}.tar.gz >> TarRepo/xmrig/xmrig/SHA256SUMS.txt", shell=True)
         call(f'cd TarRepo && git pull origin main && git add . && git commit -m "Updating xmrig to {xmrig.response}" && git push origin main', shell=True)
+        call(f"rm -rf TarRepo/tmp", shell=True)
     else:
        print("First run 'make init'")
        exit()
