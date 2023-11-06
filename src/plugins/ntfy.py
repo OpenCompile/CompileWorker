4fd37b8dc40739bcd09c263bbe0af10c20b4938d
JellyWasCoding <62903038+JellyWasCoding@users.noreply.github.com>
2023-11-06 08:20:07 +0000

    Adding ntfy plugin

---
+++ src/plugins/ntfy.py
@@ -0,0 +1,8 @@
+from subprocces import call
+
+class ntfy:
+    def __init__(self, server, topic):
+        self.server = server
+        self.topic = topic
+    def send_message(self, server, topic, message):
+        call(f"curl -d "{self.message}" {self.server}/{self.topic}", shell=True)
