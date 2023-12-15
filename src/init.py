7177ad56947f76cd23850be46aa7e6bbec03e138
JellyWasCoding <62903038+JellyWasCoding@users.noreply.github.com>
2023-12-15 13:15:28 +0000

    Fixing bugs

--- src/init.py
+++ src/init.py
@@ -4,21 +4,23 @@ import json
 import os
 
 if os.path.isfile("config.json"):
-    f = json.load(open("config.json"))
+    fi = json.load(open("config.json"))
 else:
     print("Using default config")
     call("cp config.template.json config.json", shell=True)
-    f = json.load(open("config.json"))
+    fi = json.load(open("config.json"))
+
+scriptrepo = fi["scriptsrepo"]
 
 #call(f"git clone https://github.com/{sowner}/{srepo}.git", shell=True)
 try:
-    Repo.clone_from(f"https://github.com/{f['scriptrepo']}", "BuildScripts")
+    Repo.clone_from(f"git@github.com:{scriptrepo}.git", "BuildScripts")
 except:
     print("Somthing happed skipping..")
 
 #call("git clone git@github.com:OpenCompile/TarRepo.git", shell=True)
 try:
-    Repo.clone_from(f"git@github.com:{f['finalrepo']}", "TarRepo")
+    Repo.clone_from(f"git@github.com:{fi['finalrepo']}", "TarRepo")
 except:
     repo = Repo("TarRepo")
     o = repo.remotes.origin.pull()
--- src/main.py
+++ src/main.py
@@ -7,18 +7,29 @@ from git import Repo
 import requests
 
 fi = json.load(open("config.json"))
+C = fi["gcc"]
+Cx = fi["g++"]
+cores = fi["cores"]
+
+
+#Versions of github projects
+
+xmrig_ver = requests.get(f"https://api.github.com/repos/xmrig/xmrig/releases/latest").json()['tag_name']
 
 def main():
     arch = fi["arch"]
 
-    MKFLAGS = f"CC={fi["gcc"]} CXX={fi["g++"]} CORES={fi["cores"]} ARCH={arch}"
+    MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch}"
 
     if os.path.exists("BuildScripts"):
-        # Xmrig Compile
+        # Xmrig
+        call("make -f BuildScripts/Makefile.xmrig pull", shell=True)
         call(f"make -f BuildScripts/Makefile.xmrig {MKFLAGS}", shell=True)
+        call(f"make -f BuildScripts/Makefile.xmrig package VERSION={xmrig_ver}", shell=True)
+        
 
     else:
-       print("First run 'make init'")
+       print("First run 'python3 src/init.py'")
        exit()
 if __name__ == "__main__":
     main()
