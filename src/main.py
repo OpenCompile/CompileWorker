4da5117be51e48c78bcb951d7a8c5f98eae15f04
Daniel Hejduk <jellymail@protonmail.com>
2023-12-16 12:55:56 +0100

    Fixing some bugs

--- src/main.py
+++ src/main.py
@@ -12,20 +12,36 @@ Cx = fi["g++"]
 cores = fi["cores"]
 
 
+def push_code(package, ver):
+    repo = Repo("TarRepo")
+    repo.index.add('**')
+    repo.index.commit(f"Updating {package} to {ver}")
+    repo.remotes.origin.push()
+
+
 #Versions of github projects
 
 xmrig_ver = requests.get(f"https://api.github.com/repos/xmrig/xmrig/releases/latest").json()['tag_name']
 
 def main():
+
+    try:
+        Repo.clone_from("https://github.com/xmrig/xmrig.git", "Repos/xmrig/xmrig")
+    except:
+        Repo("Repos/xmrig/xmrig").remotes.origin.pull()
     arch = fi["arch"]
 
-    MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch}"
+    if fi["isalpine"]:
+        MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch} SYS=alpine"
+    else:
+        MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch}"
 
     if os.path.exists("BuildScripts"):
         # Xmrig
         call("make -f BuildScripts/Makefile.xmrig pull", shell=True)
         call(f"make -f BuildScripts/Makefile.xmrig {MKFLAGS}", shell=True)
         call(f"make -f BuildScripts/Makefile.xmrig package VERSION={xmrig_ver}", shell=True)
+        push_code("xmrig", xmrig_ver)
         
 
     else:
--- src/plugins/ntfy.py
+++ src/plugins/ntfy.py
@@ -1,8 +1,8 @@
-from subprocces import call
+from subprocess import call
 
 class ntfy:
     def __init__(self, server, topic):
         self.server = server
         self.topic = topic
     def send_message(self, server, topic, message):
-        call(f"curl -d "{self.message}" {self.server}/{self.topic}", shell=True)
+        call(f"curl -d '{self.message}' {self.server}/{self.topic}", shell=True)
