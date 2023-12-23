f6787c2c0765276e3e629326a944aed0f708ed16
Daniel Hejduk <jellymail@protonmail.com>
2023-12-23 13:37:35 +0100

    Adding patches for xmrig

--- src/main.py
+++ src/main.py
@@ -1,23 +1,32 @@
 import os
 import json
 from subprocess import call
-from srcmanage import SourceApp
-import platform
 from git import Repo
 import requests
+import io
 
 fi = json.load(open("config.json"))
+
+if fi["ftp"]["enabled"]:
+    from ftplib import FTP
+    fftp = fi["ftp"]
+    ftp = FTP(fftp["host"], fftp["user"], fftp["password"], fftp["acct"])
+
+
 C = fi["gcc"]
 Cx = fi["g++"]
 cores = fi["cores"]
 
 
-def push_code(package, ver):
-    repo = Repo("TarRepo")
-    repo.index.add('**')
-    repo.index.commit(f"Updating {package} to {ver}")
-    repo.remotes.origin.push()
-
+def push_code(package, ver, file=False, name=False):
+    if file !=False and name !=False:
+        bio = io.BytesIO(open(file))
+        ftp.retrbinary(f'STOR {name}', bio)
+    else:
+        repo = Repo("TarRepo")
+        repo.index.add('**')
+        repo.index.commit(f"Updating {package} to {ver}")
+        repo.git.push("origin", fi["finalbranch"])
 
 #Versions of github projects
 
@@ -30,20 +39,17 @@ def main():
     except:
         Repo("Repos/xmrig/xmrig").remotes.origin.pull()
     arch = fi["arch"]
-
-    if fi["isalpine"]:
-        MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch} SYS=alpine"
-    else:
-        MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch}"
+    MKFLAGS = f"CC={C} CXX={Cx} CORES={cores} ARCH={arch}"
 
     if os.path.exists("BuildScripts"):
         # Xmrig
-        call("make -f BuildScripts/Makefile.xmrig pull", shell=True)
+        call("patch Repos/xmrig/xmrig/scripts/build.openssl.sh < patches/xmrig.patch", shell=True)
         call(f"make -f BuildScripts/Makefile.xmrig {MKFLAGS}", shell=True)
         call(f"make -f BuildScripts/Makefile.xmrig package VERSION={xmrig_ver}", shell=True)
-        push_code("xmrig", xmrig_ver)
-        
-
+        if fi["ftp"]["enabled"]:
+            push_code("xmrig", xmrig_ver, f"TarRepo/xmrig/xmrig/{xmrig_ver}", f"pub/xmrig/xmrig/{xmrig_ver}/xmrig")
+        else:
+            push_code("xmrig", xmrig_ver)
     else:
        print("First run 'python3 src/init.py'")
        exit()
